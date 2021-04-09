34b912f72cdaf206151e455840dd6da92dc94f44
Xoxol <48468468+karsar99@users.noreply.github.com>
2021-04-09 22:07:23 +0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,11 +1,16 @@
-int main() {
+#include <stdio.h>
+int main() 
+{
 	short int array[10] = {1, 2, 3, 4, 5, -6, -7, -8, -9, -10};
 	int sum = 0;
-	for(int i=0;i<10;i++){
-		if (array[i]<0){
+	for(int i=0;i<10;i++)
+	{
+		if (array[i]<0)
+		{
 			array[i] = 0 - array[i];
 		}
 		sum += array[i];
 	}
+	printf("%d\n", sum);
 	return sum;
 }
